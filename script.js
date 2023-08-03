16e9384bb9dce27af6ff3f9f7fcc29dfb87cdf2d
zhaleh <zhalehelmi@gmail.com>
2023-08-03 15:53:54 +0330

    completed project

--- script.js
+++ script.js
@@ -23,13 +23,44 @@ async function getMovies(api) {
   showMovies(res.results);
 }
 
-function showMovies() {}
+function showMovies(movies) {
+  main.innerHTML = "";
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+    const movieElm = document.createElement("div");
+
+    movieElm.classList.add("movie");
+
+    movieElm.innerHTML = `
+     <img src="${IMG_PATH + poster_path}" alt="${title}" />
+    <div class="movie-info">
+      <h3>${title}</h3>
+      <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+    </div>
+    <div class="movie-overview">
+      <h3>overview</h3>
+    ${overview}
+    </div>
+  `;
+    main.appendChild(movieElm);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const searchTerm = search.value;
-  if (searchTerm && searchTerm != "") {
+  if (searchTerm && searchTerm !== "") {
     getMovies(SEARCH_API + searchTerm);
     search.value = "";
   } else {
