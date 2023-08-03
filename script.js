8aadde1176c20c5679f3df4b12b0f4e2603439e1
zhaleh <zhalehelmi@gmail.com>
2023-08-03 15:16:01 +0330

    get Api-url img-path search-api

---
+++ script.js
@@ -0,0 +1,21 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&year=2023";
+const IMG_PATH = "https://image.tmdb.org/t/p/w500";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?query=";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjY5ZjAyZDMxZmRiMjRiY2NlZDBlMGZlMzZlYThmYyIsInN1YiI6IjY0YzhjN2Q2Zjc5NGFkMDBhZDA1N2ZmYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vqPJoxtTIyLgOmm5v9cVF8QVgjeJh2Hx0ZZV6sAaIEY",
+  },
+};
+
+getMovies(API_URL);
+
+async function getMovies(api) {
+  const data = await fetch(api, options);
+  const res = await data.json();
+  console.log(res.results);
+}
